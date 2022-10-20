12a4cc19807e9bbbb0df4a9e8e9b81316fba52d8
Екатерина Кокарева <kokarevakatja@gmail.com>
2022-10-20 14:18:43 +0300

    Update 2.0

--- src/test/java/ru/netology/SeleniumTest.java
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -32,7 +32,7 @@ public class SeleniumTest {
         }
 
         @Test
-        void shouldTestOpenWeb() {
+        void shouldPositiveTest() {
             driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
             driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79810987867");
             driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -40,6 +40,15 @@ public class SeleniumTest {
             String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
             String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
             Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    void shouldNegativeTest() {
+            driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Петр");
+            driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+            driver.findElement(By.cssSelector("[type=button]")).click();
+            String expected = "Поле обязательно для заполнения";
+            String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+            Assertions.assertEquals(expected, actual);
     }
 }
